f914bb51f6c7a5ebee9131ae5e4b99b921eef576
aljawaid <jawaid_imran@hotmail.com>
2022-10-28 05:48:59 +0100

    Add User Configuration Section to Support Page
    - Added table styling
    - Added translations

--- Template/config/support.php
+++ Template/config/support.php
@@ -1,62 +1,57 @@
-<div class="page-header">
-    <h2><?= t('Technical Support') ?></h2>
-</div>
-<div class="panel">
-    <ul>
-        <li>
-            <?= t('Official website:') ?>
-            <a href="https://kanboard.org/" target="_blank" rel="noopener noreferrer">https://kanboard.org/</a>
-        </li>
-        <li>
-            <?= t('Author:') ?>
-            <strong>Frédéric Guillot</strong> (<a href="https://github.com/kanboard/kanboard/graphs/contributors" target="_blank" rel="noopener noreferrer"><?= t('contributors') ?></a>)
-        </li>
-        <li>
-            <?= t('License:') ?>
-            <strong>MIT</strong>
-        </li>
-    </ul>
-</div>
+<?php
+    $user = $this->user->getid();
+    $user2 = $this->user->getFullname();
+?>
+    <div class="page-header" style="margin-top: 10px;">
+        <h2><?= t('Technical Support') ?></h2>
+    </div>
 
-<div class="page-header">
-    <h2><?= t('Configuration') ?></h2>
-</div>
-<div class="panel">
-    <ul>
-        <li>
-            <?= t('Application version:') ?>
-            <strong><?= APP_VERSION ?></strong>
-        </li>
-        <li>
-            <?= t('PHP version:') ?>
-            <strong><?= PHP_VERSION ?></strong>
-        </li>
-        <li>
-            <?= t('PHP SAPI:') ?>
-            <strong><?= PHP_SAPI ?></strong>
-        </li>
-        <li>
-            <?= t('HTTP Client:') ?>
-            <strong><?= Kanboard\Core\Http\Client::backend() ?></strong>
-        </li>
-        <li>
-            <?= t('OS version:') ?>
-            <strong><?= @php_uname('s').' '.@php_uname('r') ?></strong>
-        </li>
-        <li>
-            <?= t('Database driver:') ?>
-            <strong><?= DB_DRIVER ?></strong>
-        </li>
-        <li>
-            <?= t('Database version:') ?>
-            <strong><?= $this->text->e($db_version) ?></strong>
-        </li>
-        <li>
-            <?= t('Browser:') ?>
-            <strong><?= $this->text->e($user_agent) ?></strong>
-        </li>
-    </ul>
-</div>
+<!-- USER CONFIGURATION -->
+    <section class="support-section">
+        
+        <div class="table-responsive">
+            <table id="UserTable" class="support-table table-center">
+                <thead class="">
+                    <tr class="support-table-row">
+                        <th class="support-table-title" colspan="4" scope="col">
+                            <i class="fa fa-user"></i> <?= t('User Configuration') ?>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="support-table-row">
+                        <td class="cell-title"><?= t('Your Profile Full Name') ?></td>
+                        <td class="cell-value value-name" width=""><?= $this->user->getFullname() ?></td>
+                        <td class="cell-title no-border-left text-right" width="100px"><?= t('Your Role') ?></td>
+                        <td class="cell-value no-border-left text-center" width="150px"><?= $this->user->getRoleName($user['role']) ?></td>
+                    </tr>
+                    <tr class="support-table-row">
+                        <td class="cell-title"><?= t('Your User ID') ?></td>
+                        <td class="cell-value value"><?= $this->user->getid() ?></td>
+                        <td class="cell-title no-border-left text-right" width="100px"><?= t('Status') ?></td>
+                        <td class="cell-value text-center" width="150px"><?= $user2['is_active'] ? t('Active') : t('Inactive') ?></td>
+                    </tr>
+                    <tr class="support-table-row">
+                        <td class="cell-title"><?= t('Your IP Address') ?></td>
+                        <td class="cell-value value-ip" colspan="3"><?= $_SERVER['REMOTE_ADDR'] ?></td>
+                    </tr>
+                    <tr class="support-table-row">
+                        <td class="cell-title"><?= t('Current Page') ?></td>
+                        <td class="cell-value value-url" colspan="3"><?= $_SERVER['SCRIPT_URI'] ?></td>
+                    </tr>
+                    <tr class="support-table-row">
+                        <td class="cell-title"><?= t('Your Browser Name') ?></td>
+                        <td class="cell-value" colspan="3"><?= $this->helper->supportHelper->getBrowser() ?></td>
+                    </tr>
+                    <tr class="support-table-row">
+                        <td class="cell-title"><?= t('Your Browser') ?></td>
+                        <td class="cell-value" colspan="3"><?= $this->text->e($user_agent) ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
 
 <?php if (DB_DRIVER === 'sqlite'): ?>
     <div class="page-header">
