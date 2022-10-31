b63c1373859234c367e7bc1f53b9a0e397cce6c2
IJ <jawaid_imran@hotmail.com>
2022-10-31 14:45:11 +0000

    WIP #1 Add User Warnings for Sharing Info
    WIP #1 Add User Warnings for Sharing Info

---
+++ Template/config/notice.php
@@ -0,0 +1,5 @@
+Important
+
+This page shows sensitive data which should NOT be shared outside your organisation
+
+Click the button below to hide any sensitive data or visit the about page for a gitub issue template
